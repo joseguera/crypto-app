7728a566ce6e61d909a2709d2be1c373beca7569
Joseguera <joseguera@uclaextension.edu>
2022-03-17 17:18:37 -0700

    added router and routes for pages Home, Portfolio and CoinPage

---
+++ src/pages/CoinPage/CoinPage.js
@@ -0,0 +1,5 @@
+const CoinPage = () => {
+    return <div>This is the Coin Page</div>
+}
+
+export default CoinPage;
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,5 @@
+const Portfolio = () => {
+    return <div>This is the Portfolio</div>;
+  };
+  
+export default Portfolio;
