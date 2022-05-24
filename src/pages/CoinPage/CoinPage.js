4f20ce7da835d892cc3b76a55ad02b5603abbada
Joseguera <joseguera@uclaextension.edu>
2022-05-23 21:19:21 -0700

    continued to work on adding data to the individual coin page

--- src/pages/CoinPage/CoinPage.js
+++ src/pages/CoinPage/CoinPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Symbol } from "./CoinPage.styles";
 
 export default class CoinPage extends React.Component {
   state = {
@@ -28,41 +28,60 @@ export default class CoinPage extends React.Component {
     this.getCoinInfo();
   };
 
+  setDate = (dateString) => {
+    const formatDate = new Date(dateString);
+    return formatDate.toLocaleDateString('en-US');
+  } 
+
   render() {
+    const { currencyName } = this.props;
     const { profile, isLoading } = this.state;
     const hasCoinProfile = !isLoading && profile;
-    
+
     return (
       <>
         {isLoading && <div>Loading...</div>}
         {hasCoinProfile && (
-              <>
+            <>
               <h2>Your Summary:</h2>
               <div>
                 <img src={profile.image.small} alt={profile.name} />
-                <p>{profile.name}</p>
-                <Link to={`/coinpage/${profile.links.homepage[0]}`}>
+                <p>{profile.name} <Symbol>({profile.symbol})</Symbol></p>
+                <a href={profile.links.homepage[0]} target="_blank" rel="noreferrer">
                   {profile.links.homepage[0].slice(8, -1)}
-                </Link>
+                </a>
               </div>
-              <div></div>
+              <div>
+                <div>
+                  <p>ATH:</p>
+                  <p>{profile.market_data.ath[currencyName]}</p>
+                  <p>{profile.market_data.ath_change_percentage[currencyName]}%</p>
+                  <p>{this.setDate(profile.market_data.ath_date[currencyName])}</p>
+                </div>
+                <div>
+                  <p>ATL:</p>
+                  <p>{profile.market_data.atl[currencyName]}</p>
+                  <p>{profile.market_data.atl_change_percentage[currencyName]}%</p>
+                  <p>{this.setDate(profile.market_data.atl_date[currencyName])}</p>
+                </div>
+              </div>
               <div></div>
               <h2>Description</h2>
                 <div>
                   <p>{profile.description.en}</p>
                 </div>
               <div>
-                {(profile.links.blockchain_site.length !== 0) &&
-                  profile.links.blockchain_site.map((site) => {
+                {profile.links.blockchain_site.length !== 0 && (
+                  profile.links.blockchain_site.slice(0, 3).map((site) => {
                     return (
                       <div>
-                        <Link to={site}>
-                          {site.slice(8, -1)}
-                        </Link>
+                        <a href={site} target="_blank" rel="noreferrer">
+                          {site.slice(8)}
+                        </a>
                       </div>
                     )
                   })
-                }
+                )}
               </div>
             </>
           )
