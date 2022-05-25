617b5ab44422852003c3add6f760fb6a46f8bf28
Joseguera <joseguera@uclaextension.edu>
2022-05-24 21:29:27 -0700

    added last bit of data to CoinPage and some style

--- src/pages/CoinPage/CoinPage.styles.js
+++ src/pages/CoinPage/CoinPage.styles.js
@@ -3,3 +3,25 @@ import styled from "styled-components";
 export const Symbol = styled.span`
   text-transform: uppercase;
 `;
+
+export const SummaryHolder = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    text-align: center;
+`;
+
+export const Data = styled.div`
+    text-align: left;
+`;
+
+export const DataHolder = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-around;
+    width: 250px;
+`;
