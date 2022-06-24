c9a547c372d79bb2031891b0ae151b0860207a65
Jose Oseguera <67062231+joseguera@users.noreply.github.com>
2022-06-23 19:37:26 -0700

    Update NavBarButtons.js

--- src/components/NavBarButtons/NavBarButtons.js
+++ src/components/NavBarButtons/NavBarButtons.js
@@ -1,50 +1,40 @@
 import React from "react";
-import { ButtonContainer, Button, ButtonClicked, ButtonText, StyledLink } from "./NavBarButtons.styles";
+import { ButtonContainer, Button, ButtonText } from "./NavBarButtons.styles";
+import { NavLink } from "react-router-dom";
+import "./NavBarButtons.css"
 
-export default class NavBarButtons extends React.Component {
-  state = {
-    pageTabs : {
-      coins: { title: "coins", link: "/" },
-      portfolio: { title: "portfolio", link: "/portfolio" },
-    },
-    active: "coins",
-  }
+const NavBarButtons = () => {
 
-  setActive = (pageTitle) => {
-    const { coins, portfolio } = this.state.pageTabs;
-    let styled;
-    if (pageTitle === coins.title) {
-      styled = coins.title;
-    }
-    if (pageTitle === portfolio.title) {
-      styled = portfolio.title;
-    } 
-    this.setState({
-      active: styled
-    })
-  };
-  
-  render() {
-    const { pageTabs, active } = this.state;
     return (
       <ButtonContainer>
-        {Object.values(pageTabs).map((page) => {
-          return (
-            active === page.title ? (
-              <StyledLink key={page.title} to={page.link}>
-                <ButtonClicked onClick={() => this.setActive(page.title)}>
-                  <ButtonText>{page.title.charAt(0).toUpperCase() + page.title.slice(1)}</ButtonText>
-                </ButtonClicked>
-              </StyledLink>
-            ) : (
-              <StyledLink key={page.title} to={page.link}>
-                <Button onClick={() => this.setActive(page.title)}>
-                  <ButtonText>{page.title.charAt(0).toUpperCase() + page.title.slice(1)}</ButtonText>
-                </Button>
-              </StyledLink>
-            )
-        )})}
+        <NavLink
+          exact to="/"
+          style={(isActive) => ({
+           background : isActive ? "#2C2F36" : "",
+          })}
+          className="nav-link"
+        >
+          <Button>
+            <ButtonText>
+              Coins
+            </ButtonText>
+          </Button>
+        </NavLink>
+        <NavLink
+          to="/portfolio"
+          className="nav-link"
+          style={(isActive) => ({
+            background: isActive ? "#2C2F36" : "",
+          })}
+        >
+          <Button>
+            <ButtonText>
+              Portfolio
+            </ButtonText>
+          </Button>
+        </NavLink>
       </ButtonContainer>
     );
-  }
 };
+
+export default NavBarButtons;
