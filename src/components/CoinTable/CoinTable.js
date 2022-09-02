f4f5e244ea0aa067091eb37bbd1b7fc03b2b2dee
Joseguera <joseguera@uclaextension.edu>
2022-09-01 20:11:01 -0700

    moved loading variables beneath the table headings

--- src/components/CoinTable/CoinTable.js
+++ src/components/CoinTable/CoinTable.js
@@ -1,15 +1,18 @@
 import React from "react";
 import axios from "axios";
-import { TableFilters, TableContent, FilterArrowDown, FilterArrowUp } from "components";
+import {
+  TableFilters,
+  TableContent,
+  FilterArrowDown,
+  FilterArrowUp,
+} from "components";
 import { TableGrid, TableHeader } from "./CoinTable.styles";
 
 class CoinTable extends React.Component {
-  
   state = {
     coins: null,
     category: { name: "", prop: "" },
-    rowNumber: 25,
-    isLoading: false,
+    rowNumber: 10,
     hasError: false,
     filterSelection: {
       marketCapRank: {
@@ -44,7 +47,9 @@ class CoinTable extends React.Component {
   getCoins = async () => {
     try {
       this.setState({ isLoading: true });
-      const { data } = await axios( `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currencyName}${this.state.category.prop}&order=market_cap_desc&per_page=${this.state.rowNumber}&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d` );
+      const { data } = await axios(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currencyName}${this.state.category.prop}&order=market_cap_desc&per_page=${this.state.rowNumber}&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+      );
       this.setState({
         coins: data,
         isLoading: false,
@@ -71,22 +76,24 @@ class CoinTable extends React.Component {
   };
 
   setCategory = (category) => {
-    this.setState({ category: { name: category, prop: `&category=${category}` } })
-  }
+    this.setState({
+      category: { name: category, prop: `&category=${category}` },
+    });
+  };
 
   setRowQuantity = (rowNumber) => {
-    this.setState({ rowNumber })
-  }
+    console.log(rowNumber);
+    this.setState({ rowNumber });
+  };
 
   getFilteredCoins = (list, direction) => {
     return this.state.coins.sort((a, b) => {
       const first = a[list];
       const second = b[list];
       const value = direction ? first > second : second > first;
-        return value ? 1 : -1;
+      return value ? 1 : -1;
     });
   };
-
 
   componentDidUpdate(prevProps, prevState) {
     if (this.props.currencyName !== prevProps.currencyName) {
@@ -109,42 +116,40 @@ class CoinTable extends React.Component {
   }
 
   render() {
-    const { coins, isLoading, filterSelection, category, rowNumber } = this.state;
-    const hasCoins = !isLoading && coins;
+    const { coins, filterSelection, category, rowNumber } = this.state;
     return (
       <>
-        {isLoading && <div>Loading...</div>}
-        {hasCoins && (
-          <TableGrid>
-            <TableFilters setCategory={this.setCategory} category={category.name} setRowQuantity={this.setRowQuantity} rowNumber={rowNumber} />
-            {Object.values(filterSelection).map((filter) => {
-              return filter.upArrow ? (
-                <TableHeader
-                  key={filter.id}
-                  id={filter.id}
-                  onClick={() => this.setFilterArrowDirection(filter.id)}
-                >
-                  {filter.title} {<FilterArrowUp />}
-                </TableHeader>
-              ) : (
-                <TableHeader
-                  key={filter.id}
-                  id={filter.id}
-                  onClick={() => this.setFilterArrowDirection(filter.id)}
-                >
-                  {filter.title} {<FilterArrowDown />}
-                </TableHeader>
-              );
-            })}
-            <div>24h Volume/Market Cap</div>
-            <div>Circulating/Total Supply</div>
-            <div>Last 7d</div>
-            <TableContent
-              coins={coins}
-              currencyName={this.props.currencyName}
-            />
-          </TableGrid>
-        )}
+        <TableGrid>
+          <TableFilters
+            setCategory={this.setCategory}
+            category={category.name}
+            setRowQuantity={this.setRowQuantity}
+            rowNumber={rowNumber}
+          />
+          {Object.values(filterSelection).map((filter) => {
+            return filter.upArrow ? (
+              <TableHeader
+                key={filter.id}
+                id={filter.id}
+                onClick={() => this.setFilterArrowDirection(filter.id)}
+              >
+                {filter.title} {<FilterArrowUp />}
+              </TableHeader>
+            ) : (
+              <TableHeader
+                key={filter.id}
+                id={filter.id}
+                onClick={() => this.setFilterArrowDirection(filter.id)}
+              >
+                {filter.title} {<FilterArrowDown />}
+              </TableHeader>
+            );
+          })}
+          <div>24h Volume/Market Cap</div>
+          <div>Circulating/Total Supply</div>
+          <div>Last 7d</div>
+          <TableContent coins={coins} currencyName={this.props.currencyName} />
+        </TableGrid>
       </>
     );
   }
--- src/components/TableContent/TableContent.js
+++ src/components/TableContent/TableContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import _ from 'lodash';
+import _ from "lodash";
 import { Link } from "react-router-dom";
 import { roundToNumber } from "util/numberUtil";
 import {
@@ -8,7 +8,7 @@ import {
   LinkText,
   styledLink,
   PercentCell,
-  TableLine
+  TableLine,
 } from "./TableContent.styles";
 import {
   SmallGraph,
@@ -18,6 +18,9 @@ import {
 } from "components";
 
 export default class TableContent extends React.Component {
+  state = {
+    isLoading: false,
+  }
 
   formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -25,76 +28,87 @@ export default class TableContent extends React.Component {
     minimumFractionDigits: 0,
   });
 
-
   render() {
-
+    const { isLoading } = this.state;
+    const hasCoins = !isLoading && this.props.coins;
     return (
       <>
-        {this.props.coins.map((coin) => {
-          return (
-            <React.Fragment key={coin.name}>
-              <div>{coin.market_cap_rank}</div>
-              <div>
-                <Link to={`/coin/${coin.id}`} style={styledLink}>
-                  <Icon src={coin.image} alt={coin.name} />
-                  <LinkText>
-                    {coin.name} (<Symbol>{coin.symbol}</Symbol>)
-                  </LinkText>
-                </Link>
-              </div>
-              <div>{this.formatter.format(coin.current_price)}</div>
-              <PercentCell>
-                {coin.price_change_percentage_1h_in_currency > 0 ? (
-                  <UpArrowGreen />
-                ) : (
-                  <DownArrowRed />
-                )}
+        {isLoading && <div>Loading...</div>}
+        {hasCoins &&
+          this.props.coins.map((coin) => {
+            return (
+              <React.Fragment key={coin.name}>
+                <div>{coin.market_cap_rank}</div>
                 <div>
+                  <Link to={`/coin/${coin.id}`} style={styledLink}>
+                    <Icon src={coin.image} alt={coin.name} />
+                    <LinkText>
+                      {coin.name} (<Symbol>{coin.symbol}</Symbol>)
+                    </LinkText>
+                  </Link>
+                </div>
+                <div>{this.formatter.format(coin.current_price)}</div>
+                <PercentCell>
+                  {coin.price_change_percentage_1h_in_currency > 0 ? (
+                    <UpArrowGreen />
+                  ) : (
+                    <DownArrowRed />
+                  )}
+                  <div>
+                    {roundToNumber(
+                      coin.price_change_percentage_1h_in_currency,
+                      2
+                    )}
+                    %
+                  </div>
+                </PercentCell>
+                <PercentCell>
+                  {coin.price_change_percentage_24h_in_currency > 0 ? (
+                    <UpArrowGreen />
+                  ) : (
+                    <DownArrowRed />
+                  )}
                   {roundToNumber(
-                    coin.price_change_percentage_1h_in_currency,
+                    coin.price_change_percentage_24h_in_currency,
                     2
                   )}
                   %
+                </PercentCell>
+                <PercentCell>
+                  {coin.price_change_percentage_7d_in_currency > 0 ? (
+                    <UpArrowGreen />
+                  ) : (
+                    <DownArrowRed />
+                  )}
+                  {roundToNumber(
+                    coin.price_change_percentage_7d_in_currency,
+                    2
+                  )}
+                  %
+                </PercentCell>
+                <div>
+                  <ProgressBarTable
+                    currency={this.props.currencyName}
+                    values={{
+                      first: coin.total_volume,
+                      second: coin.market_cap,
+                    }}
+                  />
                 </div>
-              </PercentCell>
-              <PercentCell>
-                {coin.price_change_percentage_24h_in_currency >
-                0 ? (
-                  <UpArrowGreen />
-                ) : (
-                  <DownArrowRed />
-                )}
-                {roundToNumber(coin.price_change_percentage_24h_in_currency, 2)}
-                %
-              </PercentCell>
-              <PercentCell>
-                {coin.price_change_percentage_7d_in_currency > 0 ? (
-                  <UpArrowGreen />
-                ) : (
-                  <DownArrowRed />
-                )}
-                {roundToNumber(coin.price_change_percentage_7d_in_currency, 2)}%
-              </PercentCell>
-              <div>
-                <ProgressBarTable
-                  currency={this.props.currencyName}
-                  values={{ first: coin.total_volume, second: coin.market_cap }}
-                />
-              </div>
-              <div>
-                <ProgressBarTable
-                  currency={this.props.currencyName}
-                  values={{
-                    first: coin.circulating_supply,
-                    second: coin.total_supply,
-                  }}
-                />
-              </div>
-              <SmallGraph graphData={coin.sparkline_in_7d.price} />
-              <TableLine />
-            </React.Fragment>
-          );
-        })}
+                <div>
+                  <ProgressBarTable
+                    currency={this.props.currencyName}
+                    values={{
+                      first: coin.circulating_supply,
+                      second: coin.total_supply,
+                    }}
+                  />
+                </div>
+                <SmallGraph graphData={coin.sparkline_in_7d.price} />
+                <TableLine />
+              </React.Fragment>
+            );
+          })}
       </>
     );
   }
