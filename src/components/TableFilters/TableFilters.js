329dc308218f69e17e573d7f0c44e612d893f083
Joseguera <joseguera@uclaextension.edu>
2022-09-28 19:34:30 -0700

    finalized conversion to hooks

--- src/components/TableFilters/TableFilters.js
+++ src/components/TableFilters/TableFilters.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Categories } from 'components';
-import { TableFiltersHolder, TableFilterCategories, CategoryHolder } from './TableFilters.styles';
+import { Categories } from "components";
+import {
+  TableFiltersHolder,
+  TableFilterCategories,
+  CategoryHolder,
+} from "./TableFilters.styles";
 
-export default class TableFilters extends React.Component {
-    render() {
-        return (
-            <TableFiltersHolder>
-                <TableFilterCategories>
-                    <CategoryHolder>
-                        <Categories setCategory={this.props.setCategory} category={this.props.category} />
-                    </CategoryHolder>
-                </TableFilterCategories>
-            </TableFiltersHolder>
-        )
-    }
+export default function TableFilters(props) {
+  return (
+    <TableFiltersHolder>
+      <TableFilterCategories>
+        <CategoryHolder>
+          <Categories
+            setCategory={props.setCategory}
+            category={props.category}
+          />
+        </CategoryHolder>
+      </TableFilterCategories>
+    </TableFiltersHolder>
+  );
 }
--- src/components/ToolTip/ToolTip.js
+++ src/components/ToolTip/ToolTip.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import { TooltipHolder, CopyIcon } from "./ToolTip.styles";
 import copyIconDark from "../../images/feather-copy-dark.svg";
 import copyIconLight from "../../images/feather-copy-light.svg";
 
-export default class ToolTip extends React.Component {
-  copyIconColor =
-    this.props.selectedTheme.name === "dark-theme"
+export default function ToolTip(props) {
+  const copyIconColor =
+    props.selectedTheme.name === "dark-theme"
       ? copyIconDark
       : copyIconLight;
 
-  buttonRef = React.createRef();
-  spanRef = React.createRef();
+  const buttonRef = useRef(null);
+  const spanRef = useRef(null);
 
-  copyToClipboard = (currentRef, currentSpan) => {
+  const copyToClipboard = (currentRef, currentSpan) => {
     const copyText = currentRef.current.alt;
     navigator.clipboard.writeText(copyText);
 
@@ -21,32 +21,24 @@ export default class ToolTip extends React.Component {
     }
   };
 
-  outFunction = (currentSpan) => {
+  const outFunction = (currentSpan) => {
     if (currentSpan) {
       return (currentSpan.current.childNodes[0].textContent = "Click to Copy");
     }
   };
 
-  render() {
-    return (
-      <TooltipHolder>
-        <div
-          className="tooltip"
-          onClick={() =>
-            this.copyToClipboard(this.buttonRef, this.spanRef)
-          }
-          onMouseOut={() => this.outFunction(this.spanRef)}
-        >
-          <span className="tooltiptext" ref={this.spanRef}>
-            Click to Copy
-          </span>
-          <CopyIcon
-            ref={this.buttonRef}
-            src={this.copyIconColor}
-            alt={this.props.site}
-          />
-        </div>
-      </TooltipHolder>
-    );
-  }
+  return (
+    <TooltipHolder>
+      <div
+        className="tooltip"
+        onClick={() => copyToClipboard(buttonRef, spanRef)}
+        onMouseOut={() => outFunction(spanRef)}
+      >
+        <span className="tooltiptext" ref={spanRef}>
+          Click to Copy
+        </span>
+        <CopyIcon ref={buttonRef} src={copyIconColor} alt={props.site} />
+      </div>
+    </TooltipHolder>
+  );
 }
