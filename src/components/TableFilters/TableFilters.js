82b65d3ad08457758cf941225f38d5d9dbcdcb64
Joseguera <joseguera@uclaextension.edu>
2022-11-20 17:36:05 -0800

    removed unused functions

--- src/components/TableFilters/TableFilters.js
+++ src/components/TableFilters/TableFilters.js
@@ -7,19 +7,6 @@ import {
 } from "./TableFilters.styles";
 
 export default function TableFilters(props) {
-  const [open, setOpen] = useState(false);
-
-  const setCoinsPerPage = () => {
-    setOpen(!open);
-    // props.setCoinsPerPage()
-  };
-
-  const handleSelection = (e) => {
-    console.log(e);
-    setOpen(!open);
-    // props.setCoinsPerPage()
-  };
-
   return (
     <TableFiltersHolder>
       <TableFilterCategories>
