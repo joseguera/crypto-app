38d892f27ad337ffc4e0488be2fca8714fbde557
Jose Oseguera <67062231+joseguera@users.noreply.github.com>
2023-01-11 14:53:34 -0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,7 +25,7 @@ import searchLight from "../../images/mobile/search-light-mobile.svg";
 import searchDark from "../../images/mobile/search-dark-mobile.svg";
 import searchSelected from "../../images/mobile/search-green-mobile.svg";
 
-const Footer = (props) => {
+const Footer = ({ id, setHeader, setPagePath }) => {
   const [activeButton, setActiveButton] = useState({
     home: true,
     portfolio: false,
@@ -40,7 +40,7 @@ const Footer = (props) => {
   const portfolio = theme ? portfolioDark : portfolioLight;
   const summary = theme ? summaryDark : summaryLight;
   const searchIcon = theme ? searchDark : searchLight;
-  const idKey = props.id;
+  const idKey = id;
 
   const selected = (id) => {
     setActiveButton(() => {
@@ -59,7 +59,7 @@ const Footer = (props) => {
       }
       return selectedButton;
     });
-    props.setHeader(id)
+    setHeader(id)
   };
 
   useEffect(() => {
@@ -99,7 +99,7 @@ const Footer = (props) => {
           <Link to="/portfolio">
             <MobileIconImage
               id="portfolio"
-              onClick={(e) => {selected(e.target.id); dispatch(openCloseSearch(false)); props.setPagePath(e.target.id)}}
+              onClick={(e) => {selected(e.target.id); dispatch(openCloseSearch(false)); setPagePath(e.target.id)}}
               src={activeButton.portfolio ? portfolioSelected : portfolio}
               alt="Portfolio"
             />
